2f3f0801ffe83ea177b9d8b77c910d16d70045d0
Temurbek274 <temurbek5603@gmail.com>
2024-11-28 16:42:22 +0500

    Добавьте файлы проекта.

---
+++ Logges.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringHomework
+{
+    internal class Logger : IDisposable
+    {
+        private FileStream _fs;
+
+        public Logger()
+        {
+            _fs = new FileStream("Logger.log", FileMode.Open);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "expens.txt";
+            StringBuilder sb = new StringBuilder($" --------------------------------------\n" +
+                                                 $"| {"Expense",-10} | {"Amount",-10} | {"Date",-10} |\n" +
+                                                 $" --------------------------------------\n");
+            do
+            {
+                Console.Write("Expense name: ");
+                string str = Console.ReadLine();
+
+                Console.Write("Amount: ");
+                double amount = double.Parse(Console.ReadLine());
+
+                Console.Write("Date (15:29): ");
+                TimeSpan time = TimeSpan.Parse(Console.ReadLine());
+
+                Console.Write("\n\n");
+                Console.Write("Hit enter to add expense...");
+
+                string Stop = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(Stop))
+                {
+                    sb.AppendFormat($"| {str,-10} | {amount,-10} | {time,-10} |\n" +
+                                    $" --------------------------------------\n");
+                }
+                else if (Regex.IsMatch(Stop, "stop", RegexOptions.IgnoreCase))
+                {
+                    break;
+                }
+            } while (true);
+            try
+            {
+                File.WriteAllText(path, sb.ToString());
+                Console.WriteLine("Filega yozib qoyildi...");
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
